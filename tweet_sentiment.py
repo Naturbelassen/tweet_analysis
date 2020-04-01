c1a891a7f57f230cc3826b59354066dabc2bcea2
Naturbelassen <Nat>
2020-04-01 22:16:58 +0200

    Token and Import Modules

---
+++ tweet_sentiment.py
@@ -0,0 +1,9 @@
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET ""
+CONSUMER_KEY = ""
+CONSUMER_KEY_SECRET = ""
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
