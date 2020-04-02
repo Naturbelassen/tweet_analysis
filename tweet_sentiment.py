9481a87e9244284a16bb1cc2796477cf5e9cdf65
Naturbelassen <Nat>
2020-04-02 13:25:34 +0200

    Seperate Key and Tokens + extract tweets

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,9 +1,32 @@
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET ""
-CONSUMER_KEY = ""
-CONSUMER_KEY_SECRET = ""
+import tweepy
+import sys
+import keyToken
 
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        try:
+            print(status.text)
+            saveFile = open('twiDB.csv', 'a', encoding='utf-8')
+            saveFile.write(status.text)
+            saveFile.write('\n')
+            saveFile.close()
+            return True
+        except Exception as e:
+            print(e)
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
+            print (status)
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=['python'], is_async=True)
 
